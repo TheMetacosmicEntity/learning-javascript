5aa675ed0477da0b50dac77a556916064aadf7fc
Ash Gandawa <17383007+TheMetacosmicEntity@users.noreply.github.com>
2021-01-08 12:37:37 +0200

    Wrote the minChecker function

---
+++ 08 jan/functions.js
@@ -0,0 +1,14 @@
+const minCheck = function (a, b) {
+    //Compare A to B, return the smallest one
+    numA = parseFloat(a);
+    numB = parseFloat(b);
+    if (a === b) {
+        return "equal"
+    }
+    else if (a < b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
